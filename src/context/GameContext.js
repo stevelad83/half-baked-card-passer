716811a33145b4192ed9187e67a06f68ac7fa4a4
Steven Bryant <stevelad_83@yahoo.com>
2023-01-25 16:02:13 -0500

    added GameContext and Gameprovider

---
+++ src/context/GameContext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext } from 'react';
+const GameContext = createContext();
+const GameProvider = ({ children }) => {
+  const [selectedCard, setSelectedCard] = useState();
+
+  return (
+    <GameContext.Provider value={{ selectedCard, setSelectedCard }}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+  return context;
+};
+
+export { GameProvider, useGameContext };
